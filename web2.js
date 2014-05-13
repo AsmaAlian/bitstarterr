c94400d0e7f64fdfabb9e8537dacd366ce804487
AsmaAlian <asmaalian@std.sehir.edu.tr>
2014-05-13 21:00:57 +0000

    web2.js

---
+++ web2.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+      //response.send('Hello World 2!');
+      res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/css/:id', function(req, res) {
+  res.sendfile(__dirname + '/css/' + req.params.id);
+});
+
+app.get('/js/:id', function(req, res) {
+  res.sendfile(__dirname + '/js/' + req.params.id);
+});
+
+app.get('/html/:id' , function(req, res) {
+res.sendfile(__dirname + '/html/' + req.params.id);
+});
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
